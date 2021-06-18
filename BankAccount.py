ee716a999fc724e0ef92455406f00b6cec439cda
Jimmy Duarte <jimmy.duarte86@gmail.com>
2021-06-17 22:04:21 -0400

    Actualizacion BankAccount v2

--- BankAccount.py
+++ BankAccount.py
@@ -6,42 +6,49 @@ class BankAccount:
             self.faltante = 0
             
       
-	
+	# Metodo para hacer deposito
       def deposit(self, amount):
             self.balance += amount
             print(f"Ha depositado: {amount} pesos, su saldo final es: {self.balance} pesos")
             return self
-
+      # Metodo para hacer retiro
       def withdraw(self, amount):
             if self.balance < amount:
                   self.faltante = amount - self.balance
                   print(f"Su saldo no es suficiente, sobrepasa por {self.faltante}\nFondos insuficientes: cobrar una tarifa de $ 5 y deduzca $ 5")
             else:
                   self.balance -= amount
-                  print(f"Realizo un retiro de: {amount} pesos, su lado actualizado es de {self.balance}")
+                  print(f"Realizo un retiro de: {amount} pesos, su saldo actualizado es de {self.balance}")
             
-      
+      # Metodo para mostrar balance
       def display_account_info(self):
             print(f"Saldo final es {self.balance}")
             return self
-
+      # Metodo para sumar interes
       def yield_interest(self):
             if self.balance > 0:
-                  interes = self.balance * self.int_rate
-                  self.balance = self.balance + interes
-                  print(f"Su saldo con interes es de: {interes_final}")
+                  self.balance += self.balance * self.int_rate
+                  #interes_final = self.balance + interes
+                  print(f"Su saldo con interes es de: {self.balance}")
             else:
                   print(f"No tiene saldo")
 
-
+#Usuario 1
 jimmy = BankAccount(0.01,20000)
+#Usuario2
+esteban = BankAccount(0.02,1000)
 
-jimmy.deposit(80000)
-
-jimmy.withdraw(40000)
+#Ejecucion de Jimmy
+jimmy.deposit(80000).withdraw(40000)
 
 jimmy.yield_interest()
-
 jimmy.display_account_info()
 
+print("#################---Otro Usuario---################")
+
+#Ejecucion de Esteban
+esteban.deposit(3000).withdraw(5000)
+
+esteban.yield_interest()
+esteban.display_account_info()
 
